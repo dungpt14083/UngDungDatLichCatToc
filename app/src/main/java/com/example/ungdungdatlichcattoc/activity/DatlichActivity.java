7b04d6a609be5cfa25bff414967e68f27101be2c
Leemingchung <chungkk54@gmail.com>
2022-10-15 18:17:12 +0700

    done

--- app/src/main/java/com/example/ungdungdatlichcattoc/activity/DatlichActivity.java
+++ app/src/main/java/com/example/ungdungdatlichcattoc/activity/DatlichActivity.java
@@ -8,12 +8,9 @@ import android.widget.Spinner;
 import com.example.ungdungdatlichcattoc.R;
 
 public class DatlichActivity extends AppCompatActivity {
-   //private Spinner spAllSalon ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datlich);
-      //  spAllSalon = findViewById(R.id.sp_allSalon) ;
-
     }
 }
