1122f370480c411a10cd4a1c69be8822e8efc66a
ThangCR7 <doduythang2k@gmail.com>
2022-11-08 22:09:24 +0700

    Register successfully

--- app/src/main/java/com/example/ungdungdatlichcattoc/activity/LoginActivity.java
+++ app/src/main/java/com/example/ungdungdatlichcattoc/activity/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.ungdungdatlichcattoc.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +13,20 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.ungdungdatlichcattoc.API.ApiService;
+import com.example.ungdungdatlichcattoc.MainActivity;
 import com.example.ungdungdatlichcattoc.R;
+import com.example.ungdungdatlichcattoc.Utils;
+import com.example.ungdungdatlichcattoc.model.LoginResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
     EditText editTextPhone, editTextPassword;
@@ -29,7 +45,6 @@ public class LoginActivity extends AppCompatActivity {
         textViewRegister = findViewById(R.id.textViewRegister);
 
 
-
         textViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,16 +55,20 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = editTextPhone.getText().toString().trim();
+                String phone = editTextPhone.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
 
-                if (user != null) {
-
-                } else {
-                    Toast.makeText(LoginActivity.this, "Unregistered user, or incorrect", Toast.LENGTH_SHORT).show();
+                List<EditText> list = new ArrayList<>();
+                list.add(editTextPhone);
+                list.add(editTextPassword);
+                if (!Utils.checkValidate(list)) {
+                    return;
                 }
             }
         });
 
     }
+
+
+
 }
