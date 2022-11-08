1b47a58b7970f490d0ec369569a525853bb9b370
dungpt14083 <89771613+dungpt14083@users.noreply.github.com>
2022-11-08 14:25:08 +0700

    feature_dung_lienketman

--- app/src/main/java/com/example/ungdungdatlichcattoc/UI/Fragment_home.java
+++ app/src/main/java/com/example/ungdungdatlichcattoc/UI/Fragment_home.java
@@ -1,22 +1,53 @@
 package com.example.ungdungdatlichcattoc.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.ungdungdatlichcattoc.R;
+import com.example.ungdungdatlichcattoc.activity.BanGiaActivity;
+import com.example.ungdungdatlichcattoc.activity.DatlichActivity;
+import com.example.ungdungdatlichcattoc.activity.LichSuCutActivity;
 
 public class Fragment_home extends Fragment {
+    CardView btnDatLich,btnBangGia,btnLichSuCut;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_home,container,false);
 
+        btnDatLich = view.findViewById(R.id.home_cardview_datlich);
+        btnBangGia=view.findViewById(R.id.home_cardview_banggia);
+        btnLichSuCut = view.findViewById(R.id.home_cardview_lichsucut);
+        btnDatLich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentDatLich = new Intent(getActivity(), DatlichActivity.class);
+                startActivity(intentDatLich);
+            }
+        });
+        btnBangGia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentBangGia = new Intent(getActivity(), BanGiaActivity.class);
+                startActivity(intentBangGia);
+            }
+        });
+        btnLichSuCut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intenLichSuCut = new Intent(getActivity(), LichSuCutActivity.class);
+                startActivity(intenLichSuCut);
+            }
+        });
         return view;
     }
 }
--- app/src/main/java/com/example/ungdungdatlichcattoc/activity/LichSuCutActivity.java
+++ app/src/main/java/com/example/ungdungdatlichcattoc/activity/LichSuCutActivity.java
@@ -1,30 +1,31 @@
 package com.example.ungdungdatlichcattoc.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ungdungdatlichcattoc.MainActivity;
 import com.example.ungdungdatlichcattoc.R;
 
-public class DatlichActivity extends AppCompatActivity {
-    ImageView btnHomeBack;
+public class LichSuCutActivity extends AppCompatActivity {
+    ImageView btnhomebhack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_datlich);
-        btnHomeBack = findViewById(R.id.btnhomeDatLich);
-        btnHomeBack.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.fragment_lichsucut);
+
+        btnhomebhack = findViewById(R.id.btnhomeLichSuCut);
+        btnhomebhack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentHome = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intentHome =new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intentHome);
             }
         });
-        //fixbug
     }
 }
