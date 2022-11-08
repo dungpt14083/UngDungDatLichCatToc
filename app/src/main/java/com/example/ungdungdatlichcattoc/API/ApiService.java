23b2e03d56c04630b303af09ed0361140494868a
ThangCR7 <doduythang2k@gmail.com>
2022-11-08 22:09:24 +0700

    Register success

--- app/src/main/java/com/example/ungdungdatlichcattoc/API/ApiService.java
+++ app/src/main/java/com/example/ungdungdatlichcattoc/API/ApiService.java
@@ -1,17 +1,25 @@
 package com.example.ungdungdatlichcattoc.API;
 
+import com.example.ungdungdatlichcattoc.model.LoginResponse;
+import com.example.ungdungdatlichcattoc.model.RegisterResponse;
 import com.example.ungdungdatlichcattoc.model.Service;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiService {
     @GET("get-all")
     Call<List<Service>> getServices();
+
+    @FormUrlEncoded
+    @POST("reg")
+    Call<RegisterResponse> register(
+            @Field("phone") String phone,
+            @Field("password") String password);
+
 }
