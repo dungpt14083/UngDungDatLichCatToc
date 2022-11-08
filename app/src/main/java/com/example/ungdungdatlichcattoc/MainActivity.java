a45f7c9eeb0e1cfa3cba84ff70778bcb7da4cb41
ThangCR7 <doduythang2k@gmail.com>
2022-11-08 22:40:22 +0700

    remember login app

--- app/src/main/java/com/example/ungdungdatlichcattoc/MainActivity.java
+++ app/src/main/java/com/example/ungdungdatlichcattoc/MainActivity.java
@@ -1,45 +1,57 @@
 package com.example.ungdungdatlichcattoc;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.example.ungdungdatlichcattoc.UI.Fragment_DatLich;
 import com.example.ungdungdatlichcattoc.UI.Fragment_User;
 import com.example.ungdungdatlichcattoc.UI.Fragment_home;
+import com.example.ungdungdatlichcattoc.activity.LoginActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     ActionBar toolbar;
-    private  long Pressed;
+    private long Pressed;
     Toast mToas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = getSharedPreferences("HAIR", MODE_PRIVATE);
+        String phone = prefs.getString("phone", "");
+        String password = prefs.getString("password", "");
+
+        if (phone.equals("") && password.equals("")) {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-        if(savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,new Fragment_home()).commit();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new Fragment_home()).commit();
 
         }
         addControl();
         addEvent();
     }
+
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     Fragment selectedFragment = null;
-                    switch (item.getItemId()){
+                    switch (item.getItemId()) {
                         case R.id.frag_home:
                             selectedFragment = new Fragment_home();
                             break;
@@ -49,28 +61,29 @@ public class MainActivity extends AppCompatActivity {
                             break;
 
                     }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,selectedFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, selectedFragment).commit();
                     return true;
                 }
             };
+
     //this is add envent;
-    private  void addEvent()
-    {
+    private void addEvent() {
 
     }
-    private void addControl(){
+
+    private void addControl() {
         toolbar = getSupportActionBar();
     }
 
     @Override
     public void onBackPressed() {
-        if(Pressed + 2000 >System.currentTimeMillis()){
+        if (Pressed + 2000 > System.currentTimeMillis()) {
             mToas.cancel();
             moveTaskToBack(true);
-        }else {
-            mToas=Toast.makeText(getApplicationContext(),"ấn 2 lần để thoát",Toast.LENGTH_LONG);
+        } else {
+            mToas = Toast.makeText(getApplicationContext(), "ấn 2 lần để thoát", Toast.LENGTH_LONG);
             mToas.show();
         }
-        Pressed=System.currentTimeMillis();
+        Pressed = System.currentTimeMillis();
     }
 }
