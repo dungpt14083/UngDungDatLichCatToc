0d8939d02fee836405adb9ce3e5f73994f1b7872
ThangCR7 <doduythang2k@gmail.com>
2022-11-08 21:29:41 +0700

    Register UI app

--- app/src/main/java/com/example/ungdungdatlichcattoc/activity/LoginActivity.java
+++ app/src/main/java/com/example/ungdungdatlichcattoc/activity/LoginActivity.java
@@ -1,11 +1,7 @@
 package com.example.ungdungdatlichcattoc.activity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,20 +10,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.ungdungdatlichcattoc.API.ApiService;
-import com.example.ungdungdatlichcattoc.MainActivity;
 import com.example.ungdungdatlichcattoc.R;
-import com.example.ungdungdatlichcattoc.Utils;
-import com.example.ungdungdatlichcattoc.model.LoginResponse;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
     EditText editTextPhone, editTextPassword;
@@ -39,12 +22,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_2);
 
-
         editTextPhone = findViewById(R.id.editTextPhone);
         editTextPassword = findViewById(R.id.editTextPassword);
         buttonLogin = findViewById(R.id.buttonLogin);
 
         textViewRegister = findViewById(R.id.textViewRegister);
+
 
 
         textViewRegister.setOnClickListener(new View.OnClickListener() {
@@ -57,63 +40,16 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String phone = editTextPhone.getText().toString().trim();
+                String email = editTextPhone.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
 
-                List<EditText> list = new ArrayList<>();
-                list.add(editTextPhone);
-                list.add(editTextPassword);
-                if (!Utils.checkValidate(list)) {
-                    return;
-                }
+                if (user != null) {
 
-                loginUser(phone, password);
-            }
-        });
-
-    }
-
-
-    private void loginUser(String phone, String password) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://io.supermeo.com:8000/customer/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        ApiService api = retrofit.create(ApiService.class);
-        Call<LoginResponse> call = api.login(phone, password, getUniqueId());
-        call.enqueue(new Callback<LoginResponse>() {
-            @Override
-            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                if (response.isSuccessful()) {
-                    if (response.body() != null) {
-                        SharedPreferences.Editor editor = getSharedPreferences("HAIR", MODE_PRIVATE).edit();
-                        editor.putString("phone", phone);
-                        editor.putString("password", password);
-                        editor.putString("id", response.body().getId());
-                        editor.apply();
-                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                        finish();
-                    } else {
-                        Toast.makeText(LoginActivity.this, response.message(), Toast.LENGTH_SHORT).show();
-                    }
                 } else {
-                    Toast.makeText(LoginActivity.this, response.message(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Unregistered user, or incorrect", Toast.LENGTH_SHORT).show();
                 }
-            }
-
-            @Override
-            public void onFailure(Call<LoginResponse> call, Throwable t) {
-                Log.d("TAG", "onFailure: " + "Throw" + t.toString());
             }
         });
 
-    }
-
-    @SuppressLint("HardwareIds")
-    private String getUniqueId() {
-        return Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
     }
 }
-
-
