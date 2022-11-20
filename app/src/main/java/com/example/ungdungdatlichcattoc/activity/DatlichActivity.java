30d00274439e42b1aa6c231a79c19ebbcefc1ea7
dungpt14083 <89771613+dungpt14083@users.noreply.github.com>
2022-11-20 21:34:20 +0700

    featuer_dung_updatecustomer

--- app/src/main/java/com/example/ungdungdatlichcattoc/activity/DatlichActivity.java
+++ app/src/main/java/com/example/ungdungdatlichcattoc/activity/DatlichActivity.java
@@ -158,30 +158,34 @@ public class DatlichActivity extends AppCompatActivity {
 
     private void showDateTimeDialog(final TextView date_time_in) {
 
+
+
+
         DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 calendar.set(Calendar.YEAR, year);
                 calendar.set(Calendar.MONTH, month);
                 calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-
                 TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                         calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
-                        calendar.set(Calendar.MINUTE, minute);
 
-                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-ddHH:mm");
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
                         date_time_in.setText(simpleDateFormat.format(calendar.getTime()));
                     }
                 };
 
-                new TimePickerDialog(DatlichActivity.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show();
+                new TimePickerDialog(DatlichActivity.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY),0, false).show();
             }
         };
 
+
         new DatePickerDialog(DatlichActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+
     }
     //fixbug
 
