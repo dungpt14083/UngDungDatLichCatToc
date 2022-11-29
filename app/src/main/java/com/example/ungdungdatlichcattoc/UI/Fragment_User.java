24c28fc4d4b79d8103ccf4b6b8f85fa228fc6a22
dungpt14083 <89771613+dungpt14083@users.noreply.github.com>
2022-11-29 23:05:38 +0700

    feature_dung_updateavtar

--- app/src/main/java/com/example/ungdungdatlichcattoc/UI/Fragment_User.java
+++ app/src/main/java/com/example/ungdungdatlichcattoc/UI/Fragment_User.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,6 +20,8 @@ import androidx.cardview.widget.CardView;
 import androidx.core.app.SharedElementCallback;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.example.ungdungdatlichcattoc.API.ApiCustomer;
 import com.example.ungdungdatlichcattoc.Adapter.Adapter_user;
 import com.example.ungdungdatlichcattoc.R;
 import com.example.ungdungdatlichcattoc.activity.AccoutActivity;
@@ -25,20 +29,32 @@ import com.example.ungdungdatlichcattoc.activity.LichSuCutActivity;
 import com.example.ungdungdatlichcattoc.activity.LoginActivity;
 import com.example.ungdungdatlichcattoc.activity.RepassActivity;
 import com.example.ungdungdatlichcattoc.activity.ThongtTinHoTroKhActivity;
+import com.example.ungdungdatlichcattoc.model.ProfileCus;
 import com.example.ungdungdatlichcattoc.model.user;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Fragment_User extends Fragment {
     //    CardView crv_user_ThongTinTaiKhoan,crv_user_LichSuCut,crv_user_Repass,crv_user_thongtinhotrokhachhang;
     ListView listView;
     List<user> list;
     Adapter_user adapterUser;
+    SharedPreferences prefs;
+    String token;
+    ImageView user_img_avt_user;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
         listView = view.findViewById(R.id.lv_danhsach);
+        user_img_avt_user =view.findViewById(R.id.user_img_avt_user);
         list = new ArrayList<>();
         list.add(new user(R.drawable.ic_vt_account_32, "Thông tin khách hàng", R.drawable.ic_vt_back_16));
         list.add(new user(R.drawable.ic_vt_time_32, "Lịch sử cắt", R.drawable.ic_vt_back_16));
@@ -47,8 +63,8 @@ public class Fragment_User extends Fragment {
         list.add(new user(R.drawable.ic_vt_logout_32, "Đăng xuất", R.drawable.ic_vt_back_16));
         adapterUser = new Adapter_user(getContext(), list);
         listView.setAdapter(adapterUser);
-
-
+        getUserinfo();
+        Profile(token);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -73,6 +89,47 @@ public class Fragment_User extends Fragment {
             }
         });
         return view;
+    }
+    void getUserinfo() {
+        prefs = getActivity().getSharedPreferences("HAIR", getActivity().MODE_PRIVATE);
+        token = prefs.getString("token", toString());
+    }
+
+    private void Profile(String customerId) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://io.supermeo.com:8000/customer/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiCustomer apiCustomer = retrofit.create(ApiCustomer.class);
+        Call<ProfileCus> call = apiCustomer.Getprofile(customerId);
+        call.enqueue(new Callback<ProfileCus>() {
+            @Override
+            public void onResponse(Call<ProfileCus> call, Response<ProfileCus> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    //   cusstomerInfoList.addAll(response.body());
+                    // Log.e("TAGsize", "onResponse: " + cusstomerInfoList.size());
+                    ProfileCus profileCus = response.body();
+
+                    Glide.with(getActivity()).load("http://io.supermeo.com:8000/"+profileCus.getImage()).into(user_img_avt_user);
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                    try {
+
+
+                    } catch (Exception e) {
+
+                    }
+                    //    getdatatv();
+                } else {
+                    Log.e("loi", "onResponse: looix");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ProfileCus> call, Throwable t) {
+                //    Toast.makeText(getApplicationContext(),t.getMessage(),Toast.LENGTH_SHORT).show();
+                Log.e("loi", "onResponse: looix " + t.getMessage());
+            }
+        });
     }
     void logout(){
         SharedPreferences myPrefs = getActivity().getSharedPreferences("HAIR", Context.MODE_PRIVATE
