2c8f81a2ebd434ecc1f83cdf4847e69eca0849dc
Cuongct23 <92574437+Cuongct23@users.noreply.github.com>
2022-12-03 01:22:26 +0700

    cuong_fixgiaodienDieuKhienGiaoDichChung

---
+++ app/src/main/java/com/example/ungdungdatlichcattoc/activity/Activity_DieuKienGiaoDichChung.java
@@ -0,0 +1,16 @@
+package com.example.ungdungdatlichcattoc.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ungdungdatlichcattoc.R;
+
+public class Activity_DieuKienGiaoDichChung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dieukiengiaodichchung);
+    }
+}
