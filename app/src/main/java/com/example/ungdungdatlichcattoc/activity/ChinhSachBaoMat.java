0f3267acb994ddc146b898989e7818a14a308828
dungpt14083 <89771613+dungpt14083@users.noreply.github.com>
2022-12-04 20:21:51 +0700

    button back

--- app/src/main/java/com/example/ungdungdatlichcattoc/activity/ChinhSachBaoMat.java
+++ app/src/main/java/com/example/ungdungdatlichcattoc/activity/ChinhSachBaoMat.java
@@ -3,14 +3,32 @@ package com.example.ungdungdatlichcattoc.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.ungdungdatlichcattoc.R;
 
 public class ChinhSachBaoMat extends AppCompatActivity {
+    ImageView img_back_private;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chinhsachbaomat);
+        img_back_private = findViewById(R.id.img_back_private);
+         // ngu thì chết
+        img_back_private.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
